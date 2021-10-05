0699d18635bd537308851d5287772836cc0b1842
yohan1900 <47468230+yohan1900@users.noreply.github.com>
2021-10-05 13:30:29 +0900

    start!!(2021.10.5)

---
+++ index.js
@@ -0,0 +1,19 @@
+function eye_sound() {
+  let audio = new Audio("sound/eye.mp3");
+  audio.play();
+}
+
+function nose_sound() {
+  let audio = new Audio("sound/nose.mp3");
+  audio.play();
+}
+
+function ear_sound() {
+  let audio = new Audio("sound/ear.mp3");
+  audio.play();
+}
+
+function mouth_sound() {
+  let audio = new Audio("sound/mouth.mp3");
+  audio.play();
+}
